4841c65700fdb276c3403db6d1ca8bda9b659545
EliTheWolf <matt.carr@me.com>
2020-10-13 16:42:56 -0700

    Added UITest scene

---
+++ Assets/Scripts/UI/MoveableUIItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveableUIItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler {
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        canvasGroup.blocksRaycasts = true;
+    }
+}
