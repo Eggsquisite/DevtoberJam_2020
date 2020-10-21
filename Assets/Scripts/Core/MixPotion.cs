4a0cdd9195d172d296f94ddabaf3c9dac3d1d192
Marc Sevilleja <marc@sevilayha.com>
2020-10-20 23:58:26 -0700

    need to organizew code better

---
+++ Assets/Scripts/Core/MixPotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MixPotion : MonoBehaviour
+{
+    public float goalTemp, tempRange, tempSway, swayFreq, swayTimer;
+    public Text tempText;
+
+    private bool reset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempText.text = goalTemp.ToString();
+    }
+
+    void TempReset()
+    {
+        print("Button held down");
+        reset = !reset;
+    }
+}
